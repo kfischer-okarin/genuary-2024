d5758dcd06b516b98cd69a5b587018536e67daa1
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-01-27 14:41:50 +0900

    Water surface play around

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,40 @@
 def tick(args)
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
+  args.outputs.background_color = [0, 0, 0]
+  args.state.water_base_y ||= 500
+  args.state.water_points ||= 129.times.map { |k|
+    { x: 10 * k, y: args.state.water_base_y, f_y: 0 }
+  }
+  args.state.bubbles ||= []
+
+  if args.inputs.mouse.held
+    args.state.water_points.each do |p|
+      p[:f_y] += 20 if p[:x] > args.inputs.mouse.x - 10 && p[:x] < args.inputs.mouse.x + 10
+    end
+  end
+
+  args.state.water_points.each do |p|
+    p[:f_y] -= (p[:y] - args.state.water_base_y) / 10
+    p[:f_y] *= 0.9
+
+    p[:y] += p[:f_y]
+    if (p[:y] - args.state.water_base_y).abs < 1
+      p[:f_y] = 0
+      p[:y] = args.state.water_base_y
+    end
+  end
+
+
+  args.state.water_points.each_cons(2) do |p1, p2|
+    args.outputs.lines << p1.merge(x2: p2[:x], y2: p2[:y], r: 100, g: 100, b: 255)
+  end
+
+  args.outputs.sprites << args.state.bubbles.map { |bubble|
+    w = bubble[:w]
+    h = bubble[:h]
+    { x: bubble[:x] - (w / 2), y: bubble[:y] - (h / 2), w: w, h: h, path: 'sprites/bubble.png' }
+  }
+
+  args.outputs.labels << { x: 10, y: 710, text: $gtk.current_framerate.to_i.to_s, r: 255, g: 255, b: 255 }
 end
+
+$gtk.reset
