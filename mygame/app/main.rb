7dc7bd982ebed72043d8450d3f3de40120a35008
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-01-27 15:28:34 +0900

    Let neighboring water points influence each other

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -12,8 +12,16 @@ def tick(args)
     end
   end
 
-  args.state.water_points.each do |p|
-    p[:f_y] -= (p[:y] - args.state.water_base_y) / 10
+  water_base_y = args.state.water_base_y
+  water_points = args.state.water_points
+  water_point_count = water_points.length
+
+  water_points.each_with_index do |p, i|
+    p[:f_y] -= (p[:y] - water_base_y) / 10
+    left_p_y = i > 0 ? water_points[i - 1][:y] : water_base_y
+    right_p_y = i < water_point_count - 1 ? water_points[i + 1][:y] : water_base_y
+    mean_y = (left_p_y + right_p_y) / 2
+    p[:f_y] -= (p[:y] - mean_y) / 10
     p[:f_y] *= 0.9
 
     p[:y] += p[:f_y]
@@ -22,7 +30,6 @@ def tick(args)
       p[:y] = args.state.water_base_y
     end
   end
-
 
   args.state.water_points.each_cons(2) do |p1, p2|
     args.outputs.lines << p1.merge(x2: p2[:x], y2: p2[:y], r: 100, g: 100, b: 255)
